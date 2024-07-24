db0291b1ff322bebd735a79482f8ef1b369621b0
jd richards <jdrichardstech@gmail.com>
2024-07-24 14:37:05 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AIComponent from './AIComponent'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Open AI Component</h2>
+      <AIComponent />
+    </div>
+  )
+}
+
+export default App
